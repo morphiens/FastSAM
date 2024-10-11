0d81e15bb1c7e71dce16f97586b02651bd439d64
Morphle Brick <morphlebrick@gmail.com>
2024-10-12 02:17:10 +0530

    [Ashish] mods to fastsam

--- ultralytics/hub/utils.py
+++ ultralytics/hub/utils.py
@@ -86,60 +86,61 @@ def requests_with_progress(method, url, **kwargs):
 
 
 def smart_request(method, url, retry=3, timeout=30, thread=True, code=-1, verbose=True, progress=False, **kwargs):
-    """
-    Makes an HTTP request using the 'requests' library, with exponential backoff retries up to a specified timeout.
-
-    Args:
-        method (str): The HTTP method to use for the request. Choices are 'post' and 'get'.
-        url (str): The URL to make the request to.
-        retry (int, optional): Number of retries to attempt before giving up. Default is 3.
-        timeout (int, optional): Timeout in seconds after which the function will give up retrying. Default is 30.
-        thread (bool, optional): Whether to execute the request in a separate daemon thread. Default is True.
-        code (int, optional): An identifier for the request, used for logging purposes. Default is -1.
-        verbose (bool, optional): A flag to determine whether to print out to console or not. Default is True.
-        progress (bool, optional): Whether to show a progress bar during the request. Default is False.
-        **kwargs (dict): Keyword arguments to be passed to the requests function specified in method.
-
-    Returns:
-        (requests.Response): The HTTP response object. If the request is executed in a separate thread, returns None.
-    """
-    retry_codes = (408, 500)  # retry only these codes
-
-    @TryExcept(verbose=verbose)
-    def func(func_method, func_url, **func_kwargs):
-        """Make HTTP requests with retries and timeouts, with optional progress tracking."""
-        r = None  # response
-        t0 = time.time()  # initial time for timer
-        for i in range(retry + 1):
-            if (time.time() - t0) > timeout:
-                break
-            r = requests_with_progress(func_method, func_url, **func_kwargs)  # i.e. get(url, data, json, files)
-            if r.status_code < 300:  # return codes in the 2xx range are generally considered "good" or "successful"
-                break
-            try:
-                m = r.json().get('message', 'No JSON message.')
-            except AttributeError:
-                m = 'Unable to read JSON.'
-            if i == 0:
-                if r.status_code in retry_codes:
-                    m += f' Retrying {retry}x for {timeout}s.' if retry else ''
-                elif r.status_code == 429:  # rate limit
-                    h = r.headers  # response headers
-                    m = f"Rate limit reached ({h['X-RateLimit-Remaining']}/{h['X-RateLimit-Limit']}). " \
-                        f"Please retry after {h['Retry-After']}s."
-                if verbose:
-                    LOGGER.warning(f'{PREFIX}{m} {HELP_MSG} ({r.status_code} #{code})')
-                if r.status_code not in retry_codes:
-                    return r
-            time.sleep(2 ** i)  # exponential standoff
-        return r
-
-    args = method, url
-    kwargs['progress'] = progress
-    if thread:
-        threading.Thread(target=func, args=args, kwargs=kwargs, daemon=True).start()
-    else:
-        return func(*args, **kwargs)
+    return
+    # """
+    # Makes an HTTP request using the 'requests' library, with exponential backoff retries up to a specified timeout.
+    #
+    # Args:
+    #     method (str): The HTTP method to use for the request. Choices are 'post' and 'get'.
+    #     url (str): The URL to make the request to.
+    #     retry (int, optional): Number of retries to attempt before giving up. Default is 3.
+    #     timeout (int, optional): Timeout in seconds after which the function will give up retrying. Default is 30.
+    #     thread (bool, optional): Whether to execute the request in a separate daemon thread. Default is True.
+    #     code (int, optional): An identifier for the request, used for logging purposes. Default is -1.
+    #     verbose (bool, optional): A flag to determine whether to print out to console or not. Default is True.
+    #     progress (bool, optional): Whether to show a progress bar during the request. Default is False.
+    #     **kwargs (dict): Keyword arguments to be passed to the requests function specified in method.
+    #
+    # Returns:
+    #     (requests.Response): The HTTP response object. If the request is executed in a separate thread, returns None.
+    # """
+    # retry_codes = (408, 500)  # retry only these codes
+    #
+    # @TryExcept(verbose=verbose)
+    # def func(func_method, func_url, **func_kwargs):
+    #     """Make HTTP requests with retries and timeouts, with optional progress tracking."""
+    #     r = None  # response
+    #     t0 = time.time()  # initial time for timer
+    #     for i in range(retry + 1):
+    #         if (time.time() - t0) > timeout:
+    #             break
+    #         r = requests_with_progress(func_method, func_url, **func_kwargs)  # i.e. get(url, data, json, files)
+    #         if r.status_code < 300:  # return codes in the 2xx range are generally considered "good" or "successful"
+    #             break
+    #         try:
+    #             m = r.json().get('message', 'No JSON message.')
+    #         except AttributeError:
+    #             m = 'Unable to read JSON.'
+    #         if i == 0:
+    #             if r.status_code in retry_codes:
+    #                 m += f' Retrying {retry}x for {timeout}s.' if retry else ''
+    #             elif r.status_code == 429:  # rate limit
+    #                 h = r.headers  # response headers
+    #                 m = f"Rate limit reached ({h['X-RateLimit-Remaining']}/{h['X-RateLimit-Limit']}). " \
+    #                     f"Please retry after {h['Retry-After']}s."
+    #             if verbose:
+    #                 LOGGER.warning(f'{PREFIX}{m} {HELP_MSG} ({r.status_code} #{code})')
+    #             if r.status_code not in retry_codes:
+    #                 return r
+    #         time.sleep(2 ** i)  # exponential standoff
+    #     return r
+    #
+    # args = method, url
+    # kwargs['progress'] = progress
+    # if thread:
+    #     threading.Thread(target=func, args=args, kwargs=kwargs, daemon=True).start()
+    # else:
+    #     return func(*args, **kwargs)
 
 
 class Events:
