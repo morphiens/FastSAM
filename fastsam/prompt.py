ab7be491ce58d1410e3b36889b913ce5d628bc06
Spandan <spandan@morphle.in>
2025-01-04 18:53:14 +0530

    [Anubhav] stable branch from dagger local state

--- fastsam/prompt.py
+++ fastsam/prompt.py
@@ -514,7 +514,7 @@ class FastSAMPrompt:
             if foreground is not None:
                 intr = cv2.bitwise_and(this_mask, this_mask, mask=foreground)
                 ovap = int(100 * (cv2.countNonZero(intr) / cv2.countNonZero(this_mask)))
-                if ovap > 5:
+                if ovap > 15:
                     valid_masks.append(i)
                     logging.info(f"[Valid Mask] {i}/{len(masks)} as intersection={ovap}>5 ")
             else:
